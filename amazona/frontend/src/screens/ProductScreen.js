3ff18fc3700bccbb77075842290e7c65e78936d4
Aftab Farooq <aftabfarooq8@gmail.com>
2022-12-16 15:23:31 +0500

    Video-13-Create -Loading-Component

--- amazona/frontend/src/screens/ProductScreen.js
+++ amazona/frontend/src/screens/ProductScreen.js
@@ -9,6 +9,10 @@ import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/esm/Button';
 import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { getError } from '../utils';
+
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -41,7 +45,7 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
       }
     };
 
@@ -49,9 +53,9 @@ function ProductScreen() {
   }, [slug]);
 
   return loading ? (
-    <div>Loading...</div>
+    <LoadingBox />
   ) : error ? (
-    <div>{error}</div>
+    <MessageBox variant="danger ">{error}</MessageBox>
   ) : (
     <div>
       <Row>
@@ -62,26 +66,26 @@ function ProductScreen() {
             alt={product.name}
           ></img>
         </Col>
-        <Col md={3}>    
-        <ListGroup variant="flush">
-          <ListGroup.Item>
-            <Helmet>
-              <title>{product.name}</title>
-            </Helmet>
-            <h1>{product.name} </h1>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <Rating
-              rating={product.rating}
-              numReviews= {product.numReviews}
-            ></Rating>
-          </ListGroup.Item>
-          <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-          <ListGroup.Item>
-            Description:
-            <p>{product.description}</p>
-          </ListGroup.Item>
-        </ListGroup>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <Helmet>
+                <title>{product.name}</title>
+              </Helmet>
+              <h1>{product.name} </h1>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                rating={product.rating}
+                numReviews={product.numReviews}
+              ></Rating>
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>
+              Description:
+              <p>{product.description}</p>
+            </ListGroup.Item>
+          </ListGroup>
         </Col>
         <Col md={3}>
           <Card>
