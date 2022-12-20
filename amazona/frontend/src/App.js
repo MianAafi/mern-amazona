b5a465c360c9685f828a03c1616c3e122ee6ac1b
Aftab Farooq <aftabfarooq8@gmail.com>
2022-12-20 14:47:42 +0500

    Video-24-Save-Shipping-Address

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -13,6 +13,7 @@ import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -20,6 +21,7 @@ function App() {
   const signoutHandler = () => {
     ctxDispatch({ type: 'USER_SIGNOUT' });
     localStorage.removeItem('userInfo');
+    localStorage.removal('shippingAddress');
   };
 
   return (
@@ -57,7 +59,7 @@ function App() {
                     ></Link>
                   </NavDropdown>
                 ) : (
-                  <Link className="nav-link" to="/singin">
+                  <Link className="nav-link" to="/signin">
                     Sign In
                   </Link>
                 )}
@@ -71,6 +73,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/shipping" element={<ShippingAddressScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
